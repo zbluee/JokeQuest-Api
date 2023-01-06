33edc1afea971e166f18ff85518d83740ffb59b9
zbluee <amanueltemesgen9@gmail.com>
2023-01-06 02:54:02 -0500

    add Data transfer object for user registeration

---
+++ Models/UserRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace JobServices.Modles;
+
+public class UserRegisterDto
+{
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+}
