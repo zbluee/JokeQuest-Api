21f50d9c789e8a82a5d8311e5f8451b40b1064f6
zbluee <amanueltemesgen9@gmail.com>
2023-01-06 08:33:12 -0500

    configure Mongodb settings and register User service class to Services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using JobServices.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<UserServices>();
 
 var app = builder.Build();
 
