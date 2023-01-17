589455bb1e28be18a29010b9e7c07c889edb9412
zbluee <amanueltemesgen9@gmail.com>
2023-01-17 02:55:56 -0500

    adding custom error classes

---
+++ Error/Errors.cs
@@ -0,0 +1,28 @@
+using System.Net;
+namespace JobServices.Errors;
+
+
+public class CustomAPIException : Exception
+{
+    
+    public CustomAPIException(string message, int statusCode) : base(message) => this.HResult = statusCode;
+
+}
+
+class BadRequestException : CustomAPIException
+{
+
+    public BadRequestException(string message) : base(message, (int)HttpStatusCode.BadRequest) {}
+
+}
+
+class UnauthenticatedException : CustomAPIException
+{
+    public UnauthenticatedException(string message) : base(message, (int)HttpStatusCode.Unauthorized) {}
+}
+
+class NotFoundException : CustomAPIException
+{
+    public NotFoundException(string message) : base(message, (int)HttpStatusCode.NotFound) {}
+}
+
