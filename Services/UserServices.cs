0cce0b23796b4744034e2bd51efb6c3c5a19ad94
zbluee <amanueltemesgen9@gmail.com>
2023-01-17 03:00:54 -0500

    update user service class by adding findbyId and delete  methods

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -3,6 +3,7 @@ using JobServices.Configs;
 using MongoDB.Driver;
 using Microsoft.Extensions.Options;
 
+namespace JobServices.Services;
 public class UserServices {
 
     private readonly IMongoCollection<User> _user;
@@ -15,6 +16,8 @@ public class UserServices {
     }
 
     public async Task<List<User>> GetAllAsync() => await _user.Find(_ => true).ToListAsync();
+    public async Task DeleteAllAsync() => await _user.DeleteManyAsync(_ => true);
     public async Task CreateUser(User user) => await _user.InsertOneAsync(user);
     public async Task<User> FindUserByEmail(string email) =>  await _user.Find(user => user.Email == email).FirstOrDefaultAsync();
+    public async Task<User> FindUserById(string id) =>  await _user.Find(user => user.Id == id).FirstOrDefaultAsync();
 }
