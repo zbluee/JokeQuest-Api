2de9251eabecd882a86ade7e9c4b3ddaa8de4d46
zbluee <amanueltemesgen9@gmail.com>
2023-02-08 13:56:12 -0500

    add prop for joke collection

--- Config/MongoDBConfig.cs
+++ Config/MongoDBConfig.cs
@@ -1,10 +1,10 @@
 
-namespace JobServices.Configs;
+namespace JokeApi.Configs;
 
 public class MongoDBConfig {
     public string ConnectionString { get; set; } = null!;
     public string DatabaseName { get; set; } = null!;
     public string UserCollectionName { get; set; } = null!;
-    public string JobCollectionName { get; set; } = null!;
+    public string JokeCollectionName { get; set; } = null!;
 
 }
