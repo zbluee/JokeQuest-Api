f369ef7357a080f96e1a194ecfcbf8643712a97b
zbluee <amanueltemesgen9@gmail.com>
2023-02-08 14:00:08 -0500

    add joke model

---
+++ Models/Joke.cs
@@ -0,0 +1,22 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace JokeApi.Modles;
+
+public class Joke {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    [BsonElement("jokeQuestion")]
+    [Required]
+    public string JokeQuestion { get; set; } = null!;
+    [BsonElement("jokeAnswer")]
+    [Required]
+    public string JokeAnswer { get; set; } = null!;
+    [BsonElement("createdBy")]
+    public string? CreatedBy { get; set; } 
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+}
