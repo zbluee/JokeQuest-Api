62af2145ddf83eaddb91f56a6141ea18aa5568ab
zbluee <amanueltemesgen9@gmail.com>
2023-02-09 14:04:03 -0500

    update user services by adding new service methods for deletion and finding users

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -17,7 +17,13 @@ public class UserServices {
 
     public async Task<List<User>> GetAllAsync() => await _user.Find(_ => true).ToListAsync();
     public async Task DeleteAllAsync() => await _user.DeleteManyAsync(_ => true);
+    public async Task<User> DeleteOneAsync(string userId) {
+        var filter = Builders<User>.Filter.Where(user => user.Id == userId);
+        var user = await _user.FindOneAndDeleteAsync(filter);
+        return user;
+    } 
     public async Task CreateUser(User user) => await _user.InsertOneAsync(user);
     public async Task<User> FindUserByEmail(string email) =>  await _user.Find(user => user.Email == email).FirstOrDefaultAsync();
-    public async Task<UserRequestDto> FindUserById(string id) =>  await _user.Find(user => user.Id == id).Project(user => new UserRequestDto{Id = user.Id, Name = user.Name, Email = user.Email}).FirstOrDefaultAsync();
+    public async Task<User> FindUserByIdProjected(string id) =>  await _user.Find(user => user.Id == id).Project<User>(Builders<User>.Projection.Exclude(user => user.Password)).FirstOrDefaultAsync();
+    public async Task<User> FindUserById(string id) =>  await _user.Find(user => user.Id == id).FirstOrDefaultAsync();
 }
