75ac52201833b85fc90bfbe39e63a4a909983e66
zbluee <amanueltemesegn9@gmail.com>
2023-02-26 11:55:02 -0900

    feat: add validation for joke points field

--- Models/JokeDto.cs
+++ Models/JokeDto.cs
@@ -8,5 +8,7 @@ public class JokeDto {
     public string JokeQuestion { get; set; } = null!;
     [Required]
     public string JokeAnswer { get; set; } = null!;
+    
+    [Range(1, 10, ErrorMessage = "Joke point must be between 1 and 10")]
     public int JokePoints { get; set; } = 5;
 }
